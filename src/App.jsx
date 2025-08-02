2bc7342344d25888d4b16d733eb4a1af4f57e962
sandesh362 <sandeshbramahne.362@gmail.com>
2025-08-03 03:00:56 +0530

    updated the case sensitive

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import Blog from "./components/pages/home/Blog";
 import Admin from './pages/Admin.jsx';
 import ProtectedRoute from "./components/utils/ProtectedRoute.jsx";
 import AdminLogin from "./pages/AdminLogin.jsx"; // Fixed import path
-import Store from "./pages/Vp-store/store.jsx";  // Fixed import path
+import Store from "./pages/Vp-store/Store.jsx";  // Fixed import path
 
 const App = () => {
   return (
